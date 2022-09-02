2df389f6a7dee87d0163bbc29315433a1e2900e6
Daniela Tonello <daniela.a.tonello@gmail.com>
2022-09-02 14:21:06 -0300

    ruta en auth

--- src/main/java/com/portfolio/Daniela/Tonello/security/controller/AuthController.java
+++ src/main/java/com/portfolio/Daniela/Tonello/security/controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "")
+@CrossOrigin(origins = "https://portfolio-daniela-tonello.firebaseapp.com")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
