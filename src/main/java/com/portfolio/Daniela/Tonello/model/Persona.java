4b4f8609133c4877a173af4683a82f38301f8857
Daniela Tonello <daniela.a.tonello@gmail.com>
2022-09-02 14:01:12 -0300

    cambios en url

--- src/main/java/com/portfolio/Daniela/Tonello/model/Persona.java
+++ src/main/java/com/portfolio/Daniela/Tonello/model/Persona.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter //anotations
+@Getter @Setter
 @Entity
 public class Persona {
     
--- src/main/java/com/portfolio/Daniela/Tonello/security/controller/AuthController.java
+++ src/main/java/com/portfolio/Daniela/Tonello/security/controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://portfolio-daniela-tonello.firebaseapp.com")
+@CrossOrigin(origins = "")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
